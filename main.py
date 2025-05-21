8366a0fa999a49e0b2cf8f0a4f003c6ea6754c5c
Cezar Bretan <cezar.bretan@gmail.com>
2025-05-21 12:22:32 +0300

    playing audio

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ duration = 3  # 3 seconds of recording audio from environment
 samplerate = 16000  # 16 kHz sampling rate
 output_file = 'recorded_audio.wav'
 
-print("Recording from environment...")
+# print("Recording from environment...")
+# 
+# # Record audio
+# recording = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, dtype='int16')
+# sd.wait()  # Wait until recording is finished
+# 
+# # Save to a WAV file
+# sf.write(output_file, recording, samplerate)
+# 
+# print(f"Recording saved as {output_file}")
 
-# Record audio
-recording = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, dtype='int16')
-sd.wait()  # Wait until recording is finished
-
-# Save to a WAV file
-sf.write(output_file, recording, samplerate)
-
-print(f"Recording saved as {output_file}")
+with open("./recorded_audio.wav", "rb") as f:
+    recording, samplerate = sf.read(f)
 
 audio, fs = recording, samplerate
 # Play the audio
