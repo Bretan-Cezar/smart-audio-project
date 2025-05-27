aade256b7eee6e79a23bed801fd9f4ec201a74a8
Bretan-Cezar <cezar.bretan@gmail.com>
2025-05-27 14:36:28 +0300

    Add faster-whisper to transcribe faster

--- vol_ctrl.py
+++ vol_ctrl.py
@@ -1,9 +1,7 @@
-import torch
-from transformers import WhisperProcessor, WhisperForConditionalGeneration, pipeline
 import soundfile as sf
 import numpy as np
 import librosa
-import whisper
+from faster_whisper import WhisperModel
 
 def range_limit(vol):
     if (vol.value < 0.0):
@@ -30,20 +28,27 @@ if __name__ == "__main__":
     # model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
     # model.config.forced_decoder_ids = None
 
-    model = whisper.load_model("small")
+    model = model = WhisperModel("tiny", device="cpu", compute_type="int8")
     sample: np.ndarray
 
     with open("./recorded_audio.wav", "rb") as f:
         sample, sr = sf.read(f)
-        sample = librosa.resample(sample, orig_sr = sr, target_sr = 16000)
+        # sample = librosa.resample(sample, orig_sr = sr, target_sr = 16000)
 
     # input_features = processor(sample[:,0], sampling_rate = 16000, return_tensors="pt").input_features
     # predicted_ids = model.generate(input_features)
     # transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
 
-    result = model.transcribe("recorded_audio.wav")
+    print("Model initialized")
+    print("Transcription #1...")
+    segments, info = model.transcribe("recorded_audio.wav", beam_size=5)
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+    
+
+    print("Transcription #2...")
+    segments, info = model.transcribe("recorded_audio.wav", beam_size=5)
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
 
 
-    print(result["text"])
-
-        
